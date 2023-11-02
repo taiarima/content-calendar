d7dfa7784e7265d61e7e1d44b525ff71a22b5695
taiarima <tai.arima@colorado.edu>
2023-11-02 17:54:18 +0200

    Added data loader

--- src/main/java/com/taiarima/contentcalendar/Application.java
+++ src/main/java/com/taiarima/contentcalendar/Application.java
@@ -3,7 +3,6 @@ package com.taiarima.contentcalendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/com/taiarima/contentcalendar/config/DataLoader.java
@@ -0,0 +1,32 @@
+package com.taiarima.contentcalendar.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.taiarima.contentcalendar.model.Content;
+import org.springframework.boot.CommandLineRunner;
+import com.taiarima.contentcalendar.repository.ContentRepository;
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.InputStream;
+import java.util.List;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final ContentRepository repository;
+    private final ObjectMapper objectMapper;
+
+    public DataLoader(ContentRepository repository, ObjectMapper objectMapper) {
+        this.repository = repository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
+            repository.saveAll(objectMapper.readValue(inputStream,new TypeReference<List<Content>>(){}));
+        }
+    }
+
+}
